b941b1278011ce4ccf8983a79b547986693adbab
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2022-05-18 12:15:43 +0600

    refactored the code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
-url_template = 'https://wttr.in/{}?mTq&lang=ru'
 
-response = requests.get(url_template.format('san francisco'))
-print(response.text)
-response = requests.get(url_template.format('london'))
-print(response.text)
-response = requests.get(url_template.format('svo'))
-print(response.text)
-response = requests.get(url_template.format('Череповец'))
-print(response.text)
+url_template = 'https://wttr.in/{}'
+payload = {
+    'mTq&lang': '',
+    'lang': 'ru'
+}
+cities = ['Лондон', 'Шереметьево', 'Череповец']
+
+for city in cities:
+    response = requests.get(url_template.format(city), params=payload)
+    response.raise_for_status()
+    print(response.text)
